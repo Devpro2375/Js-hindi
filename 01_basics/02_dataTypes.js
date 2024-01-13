5bf926a131a30bb6783de9fe8c3911b9e34893cc
Devpro2375 <132826141+Devpro2375@users.noreply.github.com>
2024-01-13 11:24:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+
+console.log(3+3);
+
+console.log(5+2);
+
+console.log(59+56);
+
+console.log(565+665);
+
+// imp documantation tc39.es And mdn
+
+let name = "Devesh" // string data type
+let age = 20 // number
+
+// datatypes
+
+// number => 2 to power 53
+// bigint for big numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
+
+
